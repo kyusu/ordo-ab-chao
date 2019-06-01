928dbe262524bf7777bd591b216ed42d0af898ec
急須 <feedmewithspam@web.de>
2019-06-01 21:50:21 +0200

    Expose classifiers

---
+++ index.js
@@ -0,0 +1,19 @@
+const isBlackMetal = require('./src/isBlackMetal.js');
+const isDeathMetal = require('./src/isDeathMetal.js');
+const isDoomMetal = require('./src/isDoomMetal.js');
+const isHeavyMetal = require('./src/isHeavyMetal.js');
+const isMetal = require('./src/isMetal.js');
+const isPowerMetal = require('./src/isPowerMetal.js');
+const isSpeedMetal = require('./src/isSpeedMetal.js');
+const isThrashMetal = require('./src/isThrashMetal.js');
+
+module.exports = {
+    isBlackMetal,
+    isDeathMetal,
+    isDoomMetal,
+    isHeavyMetal,
+    isMetal,
+    isPowerMetal,
+    isSpeedMetal,
+    isThrashMetal
+};
